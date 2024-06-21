c585c23097e2175b70b2a54f0e20b5149b32988b
Charckle <andrej.zubin@email.com>
2024-06-21 23:05:38 +0200

    added zovnek and zuzemberg castle...and AI that defends its archers, if they are pinned

--- AI/basic_ai_v1/ai_v_1.gd
+++ AI/basic_ai_v1/ai_v_1.gd
@@ -27,8 +27,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if state_ == 2:
-		manage_defense()
+	
 	empty_dead_units()
 	
 func print_units_groups():
@@ -64,6 +63,8 @@ func set_state(my_side, their_side):
 
 func _on_timer_timeout():
 	evaluate_threat()
+	if state_ == 2:
+		manage_defense()
 
 func initial_setup():
 	var units_on_map = root_map.get_all_units()
@@ -119,6 +120,8 @@ func manage_defense():
 	if initial_units_to_markers == false:
 		move_to_initial_markers()
 		initial_units_to_markers = true
+	
+	check_range_units_pinned()
 
 func move_to_initial_markers():
 	for unit_id in markers:
@@ -157,6 +160,7 @@ func check_being_shot_at():
 	# units should have I AM BEING SHOT AT variable
 	# if a unit is being shot at, send that unit to attack the source
 	# check sorounding cells of the attacking unit to see how many forces to send
+	# or to send archers
 	pass
 
 func check_range_units_pinned():
@@ -173,7 +177,45 @@ func check_range_units_pinned():
 					pinned_unit = unit_wr
 			if pinned_unit != null:
 				# check which unit is pinning it at attack
-				pass
+				var pinning_unit_wr = gr(pinned_unit).units_pining_me[0]
+				# all group target that guy
+				group_sttack_unit(group, pinning_unit_wr)
+				# and the closest other group
+				var group_helping = get_neerest_group_help(pinned_unit, group)
+				group_sttack_unit(group_helping, pinning_unit_wr)
+
+func get_neerest_group_help(unit_in_need_wr, group_seeking_help):
+	var groups_by_distance = []
+	var unit_in_need_obj = gr(unit_in_need_wr)
+	
+	for type_group in unit_groups:
+		for group in unit_groups[type_group]:
+			if group != group_seeking_help:
+				var units_by_distance = []
+				
+				for unit_wr in group["units"]:
+					var unit_obj = gr(unit_wr)
+					if unit_obj != null:
+						var distance = int(unit_in_need_obj.global_position.distance_to(unit_obj.global_position))
+						units_by_distance.append([unit_obj, distance])
+					
+					units_by_distance.sort_custom(func(a, b): return a[1] < b[1])
+					groups_by_distance.append([group, units_by_distance[0][1]])
+	
+	groups_by_distance.sort_custom(func(a, b): return a[1] < b[1])
+	# check if they are not defending a pinning yet
+	# ...
+	
+	return groups_by_distance[0][0]
+
+
+func group_sttack_unit(group, unit_to_attack_wr):
+	for unit_wr in group["units"]:
+		var unit = gr(unit_wr)
+		var unit_to_attack = gr(unit_to_attack_wr)
+		if unit != null and unit_to_attack != null:
+			unit.set_attack(unit_to_attack_wr)
+	
 
 func empty_dead_units():
 	# remove dead units from control groups
