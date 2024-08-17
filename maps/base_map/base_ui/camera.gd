26f53f57395a99f9ff3961daaaef15497957c355
Charckle <andrej.zubin@email.com>
2024-08-17 15:01:08 +0200

    added scroll on cornerbump + small fix for door

--- maps/base_map/base_ui/camera.gd
+++ maps/base_map/base_ui/camera.gd
@@ -15,6 +15,9 @@ signal start_move_selection
 @onready var map_root = get_tree().root.get_child(1)
 #var speed := 20.0
 
+var edge_threshold = 20  # Distance from the edge to start moving the camera
+var camera_speed = 400    # Speed at which the camera moves
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	draw_area(false)
@@ -27,7 +30,9 @@ func _process(delta):
 	var inpy = (int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
 	#position.x += inpx * speed * delta
 	position.x = lerp(position.x, position.x + inpx * speed, speed * delta)	
-	position.y = lerp(position.y, position.y + inpy * speed, speed * delta)	
+	position.y = lerp(position.y, position.y + inpy * speed, speed * delta)
+	
+	move_camera_if_bumped(delta)
 	
 func _input(event):
 	if event is InputEventMouse:
@@ -64,3 +69,28 @@ func draw_area(s=true):
 	pos.y = min(startV.y, endV.y)
 	selectionbox.position = pos
 	selectionbox.size *= int(s)
+
+func move_camera_if_bumped(delta):
+	var viewport_size = get_viewport_rect().size
+	var mouse_position = get_viewport().get_mouse_position()
+	
+	var camera_movement = Vector2()
+
+	# Check if the mouse is near the left edge
+	if mouse_position.x <= edge_threshold:
+		camera_movement.x -= camera_speed * delta
+
+	# Check if the mouse is near the right edge
+	if mouse_position.x >= viewport_size.x - edge_threshold:
+		camera_movement.x += camera_speed * delta
+
+	# Check if the mouse is near the top edge
+	if mouse_position.y <= edge_threshold:
+		camera_movement.y -= camera_speed * delta
+
+	# Check if the mouse is near the bottom edge
+	if mouse_position.y >= viewport_size.y - edge_threshold:
+		camera_movement.y += camera_speed * delta
+
+	# Move the camera
+	position += camera_movement
