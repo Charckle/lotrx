86759e71374c837acc037dfe0fdba876d4585572
Janez Novak <banana@banana.com>
2026-02-15 18:00:02 +0100

    added spawners

--- spawners/units/units_spawner.gd
+++ spawners/units/units_spawner.gd
@@ -5,8 +5,8 @@ extends Node2D
 @export var unit_type = 0
 @export var unit_numbers = 1
 @onready var root_map = get_tree().root.get_node("game")
-@onready var title_map = root_map.get_node("TileMap")
-@onready var my_position_2i = title_map.local_to_map(global_position)
+@onready var tile_map_layer = root_map.get_node("TileMap/base_layer")
+@onready var my_position_2i = tile_map_layer.local_to_map(global_position)
 
 @onready var peasant = preload("res://units/other_units/peasant/peasant.tscn")
 @onready var archer = preload("res://units/other_units/archer/archer.tscn")
@@ -16,24 +16,64 @@ extends Node2D
 @onready var pikeman = preload("res://units/other_units/pikeman/pikeman.tscn")
 @onready var swordsman = preload("res://units/other_units/swordsman/swordsman.tscn")
 
+#0	Peasant
+#1	Archer
+#2	Crossbow
+#3	Knight
+#4	Maceman
+#5	Pikeman
+#6	Swordsman
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn()
 	
 
 func spawn():
-	#next_cell_global = title_map.map_to_local(next_cell)
-	#if astar_grid.is_point_solid(next_cell):
-	var place_position = my_position_2i
-	for num in range(unit_numbers):
-		print(my_position_2i)
+	var offsets = _get_spawn_offsets(unit_numbers)
+	for offset in offsets:
+		var tile_pos = my_position_2i + offset
 		var instance = get_unit_().instantiate()
-		instance.position = title_map.map_to_local(my_position_2i)
+		instance.position = tile_map_layer.map_to_local(tile_pos)
 		instance.faction = faction
 		root_map.get_node("units").add_child(instance)
-		my_position_2i.x += 1
-		print("added")
-		
+	queue_free()
+
+
+# Returns tile offsets from center in formation order:
+#   Row 0: center, right, left  ->  Row 1: center, right, left
+#   Then expand both rows outward: +2/-2 on row 0, +2/-2 on row 1, etc.
+func _get_spawn_offsets(count: int) -> Array:
+	var offsets = []
+	if count <= 0:
+		return offsets
+
+	# Phase 1: Row 0 center-out (center, right, left)
+	var initial_x = [0, 1, -1]
+	for x in initial_x:
+		if offsets.size() >= count:
+			break
+		offsets.append(Vector2i(x, 0))
+
+	# Phase 2: Row 1 center-out (center, right, left)
+	for x in initial_x:
+		if offsets.size() >= count:
+			break
+		offsets.append(Vector2i(x, 1))
+
+	# Phase 3: Expand outward on both rows, left and right
+	var x_dist = 2
+	while offsets.size() < count:
+		for row in range(2):
+			if offsets.size() >= count:
+				break
+			offsets.append(Vector2i(x_dist, row))
+			if offsets.size() >= count:
+				break
+			offsets.append(Vector2i(-x_dist, row))
+		x_dist += 1
+
+	return offsets
 
 func get_unit_():
 	var units_builders = {
